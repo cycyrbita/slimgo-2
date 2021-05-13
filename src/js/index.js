5035aa656c1034aa07daca29e6ac75df1743c109
Nikita <cycyrbita@mail.ru>
2021-05-14 01:50:42 +0300

    логика для блока quest

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function () {
+    quest();
     Start();
 
     $('.js-commits__box').owlCarousel({
@@ -42,4 +43,57 @@ $(document).ready(function () {
             timerId = setTimeout(tick, 1000);
         }, 1000);
     };
+
+    function quest() {
+        $('.quest__check').click(function() {
+            $(this).parent().children('.quest__check').removeClass('active');
+            $(this).addClass('active');
+            $(this).parent().addClass('ready');
+        });
+
+        $('.quest__btn.next').click(function() {
+            if($('.quest__item').last().prev().hasClass('visible')) {
+                $('.quest__btn.next').removeClass("visible");
+                $('.quest__btn.status').addClass("visible");
+            }
+            $('.quest__item.visible.ready').removeClass('visible').next().addClass('visible');
+        });
+
+        $('.quest__btn.prev').click(function() {
+            if($('.quest__item').first().hasClass('visible')) {
+                
+            } else {
+                $('.quest__item.visible').removeClass('visible').prev().removeClass('hide').addClass('visible');
+            }
+
+            if($('.quest__item').last().prev().hasClass('visible')) {
+                $('.quest__btn.next').addClass("visible");
+                $('.quest__btn.status').removeClass("visible");
+            }
+        });
+
+        $('.quest__btn.status').click(function() {
+            if($('.quest__item').last().hasClass('ready')) {
+                $('.section_quest').hide();
+
+                if($('.quest__check.count-1').hasClass('active')) {
+                    $('.section_result').fadeIn();
+                    $('.result').hide();
+                    $('.result_1').show();
+                } else if($('.quest__check.count-2').hasClass('active')) {
+                    $('.section_result').fadeIn();
+                    $('.result').hide();
+                    $('.result_2').show();
+                } else if($('.quest__check.count-3').hasClass('active')) {
+                    $('.section_result').fadeIn();
+                    $('.result').hide();
+                    $('.result_3').show();
+                } else {
+                    $('.section_result').fadeIn();
+                    $('.result').hide();
+                    $('.result_4').show();
+                }
+            }
+        });
+    }
 });
